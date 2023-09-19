78955685f4c8a5c98fbcb8b63d5f1fe7b9ddc471
s_a_j_a_g <spd.sajag@gmail.com>
2023-09-19 16:33:15 +0545

    OOP

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,34 @@
-console.log("Hello");
+// console.log("Hello");
+
+class Player {
+  name: string;
+  health: number;
+  isInvincible: boolean;
+  crush: Player;
+
+  greet() {
+    console.log(`Hello! My name is ${this.name}.`);
+  }
+
+  displayCrush() {
+    console.log(`${this.name}: My crush is ${this.crush.name}.`);
+  }
+}
+
+const mario = new Player();
+mario.name = "Mario";
+mario.health = 10;
+mario.isInvincible = true;
+
+const peach = new Player();
+peach.name = "Peach";
+peach.health = 8;
+peach.isInvincible = true;
+
+mario.greet();
+peach.greet();
+
+mario.crush = peach;
+mario.displayCrush();
+
+console.log("Mario's crush health: ", mario.crush.health);
